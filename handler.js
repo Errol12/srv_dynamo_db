8adb45c1202f08fe774dc2602c2574b4fbd2aab9
Errol <errolf07@gmail.com>
2019-05-07 22:30:33 +0530

    first commit

---
+++ handler.js
@@ -0,0 +1,54 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+const addProduct = require('./handlers/create');
+const viewProduct = require('./handlers/view');
+const listProducts = require('./handlers/list');
+const removeProduct = require('./handlers/remove');
+
+const create = (event, context, callback) => {
+    const data = JSON.parse(event.body);
+    addProduct(data)
+        .then(result => {
+            const response = { body: JSON.stringify(result) };
+            callback(null, response);
+        })
+        .catch(callback);
+};
+const list = (event, context, callback) => {
+    listProducts()
+        .then(result => {
+            const response = { headers: { 
+                "Access-Control-Allow-Origin": "*" 
+            },body: JSON.stringify(result) };
+            callback(null, response);
+        })
+        .catch(callback);
+};
+
+const view = (event, context, callback) => {
+    viewProduct(event.pathParameters.id)
+        .then(result => {
+            const response = { body: JSON.stringify(result) };
+            callback(null, response);
+        })
+        .catch(callback);
+};
+
+
+const remove = (event, context, callback) => {
+    removeProduct(event.pathParameters.id)
+        .then(result => {
+            const response = { body: JSON.stringify({message: 'Product removed.'}) };
+            callback(null, response);
+        })
+        .catch(callback);
+};
+
+
+module.exports = {
+    create,
+    view,
+    remove,
+    list
+};
---
+++ readme.txt
@@ -0,0 +1,1 @@
+https://blog.pusher.com/rest-api-serverless-framework/
